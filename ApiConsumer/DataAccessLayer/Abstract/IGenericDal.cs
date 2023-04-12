2e298a792056651a68400b78c37dde983df9a585
Yıldıray Duran <duranyildirayy@gmail.com>
2023-04-12 22:07:21 +0300

    Migration,DataAccessLayer Katmani Tamamlandi

---
+++ ApiConsumer/DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<Entity> where Entity : class
+    {
+        void Insert(Entity entity);
+        void Delete(Entity entity);
+        void Update(Entity entity);
+        List<Entity> GetList();
+        Entity GetById(int id);
+    }
+}
---
+++ ApiConsumer/EntityLayer/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class Service
+    {
+        public int ServiceID { get; set; }
+        public string ServicIcon { get; set; }
+        public string Title { get; set; }
+        public string ServiceDesc { get; set; }
+    }
+}
